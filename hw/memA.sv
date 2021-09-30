90827f7b5b2519323bfa627403f64c2d210aa85d
Ryan Hemmila <ryanhemmila@gmail.com>
2021-09-29 20:04:37 -0500

    fix

--- hw/memA.sv
+++ hw/memA.sv
@@ -11,10 +11,10 @@ module memA
   );
   
   generate
-    genvar row;
+    genvar row, i, j;
     for (row = 0; row < DIM; row = row + 1) begin : iter
       if (row == 0) begin
-        transpose_fifo #(.DEPTH(DIM + row), .BITS(BITS_AB)) queue (
+        transpose_fifo #(.DEPTH(DIM), .BITS(BITS_AB)) queue (
           .clk(clk),
           .rst_n(rst_n),
           .en(en && ~WrEn),
@@ -22,15 +22,22 @@ module memA
           .d(Ain),
           .q(Aout[row])
         );
-      end else begin 
+      end else begin
+        wire [BITS_AB-1:0] queue_in [DIM + row - 1:0];
         transpose_fifo #(.DEPTH(DIM + row), .BITS(BITS_AB)) queue (
           .clk(clk),
           .rst_n(rst_n),
           .en(en && ~WrEn),
           .WrEn((Arow == row) && WrEn),
-          .d({{row{8'h00}}, Ain}),
+          .d(queue_in),
           .q(Aout[row])
         );
+        for (i = 0; i < row; i = i + 1) begin : zero_iter
+          assign queue_in[i] = 8'h00;
+        end
+        for (j = 0; j < DIM; j = j + 1) begin : ain_iter
+          assign queue_in[row + j] = Ain[j];
+        end
       end
     end
   endgenerate
